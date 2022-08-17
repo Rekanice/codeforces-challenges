349fafb075e9af27bf03afa099c3f42e0558a83c
Rekanice <62833151+Rekanice@users.noreply.github.com>
2022-08-18 07:31:14 +0800

    Create watermelon.py

---
+++ watermelon.py
@@ -0,0 +1,8 @@
+# Problem link : https://codeforces.com/contest/4/problem/A
+# my submission: https://codeforces.com/contest/4/submission/168718236
+
+w = int(input())
+if w%2 == 0 and w!=2:
+    print("YES")
+else:
+    print("NO")
